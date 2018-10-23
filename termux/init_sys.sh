c426162af474943e3fb8699fb40a0141e3b0fd37
linweifu <linweifu@rotai.com>
2018-10-23 10:24:40 +0800

    修改termux zsh安装脚本

--- termux/init_sys.sh
+++ termux/init_sys.sh
@@ -3,5 +3,5 @@ apt update
 apt upgrade -y
 apt install -y curl vim git python openssh dnsutils
 #install zsh
-apt install -y zsh-doc zsh-common zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
+chsh zsh
