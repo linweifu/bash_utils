1d3afddcdcffa7797db3489ad7bb8aef7909d2c6
linweifu <linweifu@live.cn>
2018-11-04 10:57:21 +0800

    Update init_sys.sh

--- termux/init_sys.sh
+++ termux/init_sys.sh
@@ -2,6 +2,7 @@
 apt update
 apt upgrade -y
 apt install -y curl vim git python openssh dnsutils
+pip install --upgrade pip
 #install zsh
 bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
 chsh zsh
